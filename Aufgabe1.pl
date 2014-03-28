c132fb882e8505f41326b58d2bd52eaef99206dd
Timmay24 <tim.hartig@haw-hamburg.de>
2014-03-28 16:37:08 +0100

    Update.

--- Aufgabe1.pl
+++ Aufgabe1.pl
@@ -3,4 +3,38 @@
 
 % AUFGABE 1 %
 
-% Teil 1
+% Teil 1.1
+
+% Teil 1.2
+
+% Teil 1.3
+
+% Teil 1.4
+
+% Teil 1.5
+
+% Teil 1.6
+          List Even Odd
+eo_count([],   0,   0  ).
+
+% Zählt Elemente einer Liste %
+elem_count([],0).
+elem_count([_|Rest],Accu) :- elem_count(Rest,NewAccu), Accu is NewAccu + 1.
+
+
+
+% Teil 1.7
+
+% Teil 1.8
+
+
+
+% Teil 2.1
+
+% Teil 2.2
+
+% Teil 2.3
+
+% Teil 2.4
+
+% Teil 2.5
