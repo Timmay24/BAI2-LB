1f6d707119812e37d412eb24b7a3a344143a4070
Tim Hartig <tim.hartig@haw-hamburg.de>
2014-04-02 19:55:14 +0200

    Big update to Prolog A1

--- Aufgabe1.pl
+++ Aufgabe1.pl
@@ -4,10 +4,14 @@
 % AUFGABE 1 %
 
 % Teil 1.1
-is_a_list([_|_]).
+is_a_list([]).     % Eingabe ist eine leere Liste
+is_a_list([_|_]).  % Eingabe ist eine Liste beliebiger Länge
+
 % Teil 1.2
-app([],L,L).
-app([KopfL1|RestL1],L2,[KopfL1|RestL3]) :- app(RestL1,L2,RestL3).
+% app(Liste 1, Liste 2, Ergebnisliste)
+app([],L,L). % Basisfall, Liste 2 mit Ergebnisliste unifizieren
+app([KopfL1|RestL1],L2,[KopfL1|RestL3]) :- %
+    app(RestL1,L2,RestL3).                 %
 
 % Teil 1.3
 infix(I,[I|_]).
@@ -21,25 +25,31 @@ suffix(S,[_|Rest]) :- suffix(S,Rest).
 prefix(P,[P|_]).
 
 % Teil 1.6
-%        List Even Odd
-eo_count([],  0,   0  ).
+% eo_count(Eingabeliste L, Zähler geradestelliger Listen Even, Zähler ungeradest. Listen Odd)
+eo_count([],0,0).       % Basisfall
 
-% Zählt Elemente einer Liste %
-elem_count([],0).
-elem_count([_|Rest],Accu) :- elem_count(Rest,NewAccu), Accu is NewAccu + 1.
 
 % Teil 1.7
-del_element(_E,[],[]).
-del_element(E,[LK|LR],[RK|RR]).
+% del_element(Zu löschendes Element E, Eingabeliste L, Ergebnisliste R)
+del_element(_E,[],[]).        % Basisfall, Rek.-Abbruch
+del_element(E,[E|Lrest],R) :- % Ist E mit dem Kopf der Liste L unifizierbar
+    del_element(E,Lrest,R).     % Ergebnisliste R ohne E "weitergeben"
+
+                                      % andernfalls
+del_element(E,[L_Kopf|L_Rest],Out) :- % Liste R mit Kopf von L aufbauen
+    del_element(E,L_Rest,R_Rest),       % Rekursiver Aufruf (result. Liste R auf Rückweg)
+    Out = [L_Kopf|R_Rest].              % Liste R gebunden an Out
 
 
 % Teil 1.8
-substitute(_E1,_E2,[],[]).                                        /*
-substitute(E1,E2,[LK|LR],[|RR]) :- substitute(E1,E2,LR,RR),LK=E1. */
+substitute(_E1,_E2,[],[]).
 
 
 % Teil 2.1
-a21(OutDEPTNO, OutDNAME) :- dept(OutDEPTNO, OutDNAME, 'BOSTON').
+a21 :- dept(DEPTNO, DNAME, 'BOSTON')               % Passende Werte zu Boston an Variablen binden
+    write('DeptNo: '), write(DEPTNO),              % Ausgabe Deptno
+    write(' ; DeptName: '), writeln(DNAME), fail.  % Ausgabe DeptName
+a21.                                               % false durch fail verhindern
 
 % Teil 2.2
 a22(DNAME,EMPNO,ENAME) :-
@@ -47,16 +57,57 @@ a22(DNAME,EMPNO,ENAME) :-
     emp(EMPNO,ENAME,_JOB,_MGR,_SAL,_EXT,DEPTNO).
 
 % Teil 2.3
-a23(OutENAME) :- emp(_EMPNO,OutENAME,'PRESIDENT',_MGR,_SAL,_EXT,_DEPTNO).
-a23(OutENAME) :- emp(_EMPNO,OutENAME,'MANAGER',_MGR,_SAL,_EXT,_DEPTNO).
+a23 :- emp(_EMPNO,ENAME,'PRESIDENT',_MGR,_SAL,_EXT,_DEPTNO), % Präsidenten suchen
+    write('President: '), writeln(ENAME), fail.              % Job + Namen Ausgabe
+    
+a23 :- emp(_EMPNO,ENAME,'MANAGER',_MGR,_SAL,_EXT,_DEPTNO),   % Mananger suchen
+    write('Manager: '), writeln(ENAME), fail.                % Job + Namen Ausgabe
+a23.
 
 % Teil 2.4
-a24(OutENAME) :-
-    emp(_,OutENAME,_,MGR,ESAL,_,_),emp(MGR,_,_,_,MSAL,_,_),ESAL > MSAL.
+a24 :- emp(_EMPNO,ENAME,_,MGR,ESAL,_EEXT,_EDEPTNO), % Mitarbeiter samt Gehalt und Vorgesetzten laden
+    emp(MGR,MGRNAME,_,_,MSAL,_MEXT,_MDEPTNO),       % Vorgesetzten des MA samt Gehalt laden
+    ESAL > MSAL,                                    % Gehalt des MA > Gehalt des Vorgesetzten?
+    write(ENAME), write(' verdient mehr als sein Vorgesetzter '), writeln(MGRNAME), fail.
+a24.
 
 % Teil 2.5
-a25(MGRNAME,OutENAME) :-
-    emp(EMPNO,MGRNAME,_,_,_,_,_), emp(EMPNO2,OutENAME,_,EMPNO,_,_,_).
+a25(MGRNAME) :-
+    %
+    emp(MGRNO,MGRNAME,_,_,_,_,_), emp(EMPNO,EMPNAME,_,MGRNO,_,_,_), a25(EMPNAME),
+    write(EMPNAME), write(' ist '), write(MGRNAME), writeln(' unterstellt.'), fail.
+a25(_MGRNAME).
 
 
-%emp(EMPNO,MGRNAME,_,_,_,_,_), emp(_,ENAME,_,EMPNO,_,_,_).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                          %
+% Datenbank aus empdept.pl %
+%                          %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%emp(EMPNO, ENAME, JOB, MGR, SAL, EXT, DEPTNO).
+emp(7654, 'MARTIN', 'SALESMAN', 7698, 1250, 1400, 30).
+emp(7499, 'ALLEN', 'SALESMAN', 7698, 1600, 300, 30).
+emp(7844, 'TURNER', 'SALESMAN', 7698, 1500, 0, 30).
+emp(7521, 'WARD', 'SALESMAN', 7698, 1250, 500, 30).
+emp(7839, 'KING', 'PRESIDENT', null, 5000, null, 10).
+emp(7698, 'BLAKE', 'MANAGER', 7839, 2850, null, 30).
+emp(7782, 'CLARK', 'MANAGER', 7839, 2450, null, 10).
+emp(7566, 'JONES', 'MANAGER', 7839, 2975, null, 20).
+emp(7900, 'JAMES', 'CLERK', 7698, 950, null, 30).
+emp(7902, 'FORD', 'ANALYST', 7566, 3000, null, 20).
+emp(7369, 'SMITH', 'CLERK', 7902, 800, null, 20).
+emp(7788, 'SCOTT', 'ANALYST', 7566, 3000, null, 20).
+emp(7876, 'ADAMS', 'CLERK', 7788, 1100, null, 20).
+emp(7934, 'MILLER', 'CLERK', 7782, 1300, null, 10).
+
+%dept(DEPTNO, DNAME, LOC).
+dept(10, 'ACCOUNTING', 'NEW YORK').
+dept(20, 'RESEARCH', 'DALLAS').
+dept(30, 'SALES', 'CHICAGO').
+dept(40, 'OPERATIONS', 'BOSTON').
+
+
+% Zählt Elemente einer Liste %
+elem_count([],0).
+elem_count([_|Rest],Accu) :- elem_count(Rest,NewAccu), Accu is NewAccu + 1.
